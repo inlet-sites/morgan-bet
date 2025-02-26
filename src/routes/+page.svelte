b93e637e26bc987d2265fea857ba69d36ad09fba
Lee Morgan <lee@leemorgan.dev>
2025-02-25 19:07:36 -0500

    Create the landing page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import "$lib/global.css";
+</script>
+
+<div class="container">
+    <h1>Morgan Bet</h1>
+
+    <div class="buttons">
+        <a class="button" href="/register">Sign Up</a>
+        <a class="button" href="/signin">Sign In</a>
+    </div>
+</div>
+
+<style>
+    .container{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background: var(--platinum);
+        height: 100vh;
+        width: 100vw;
+    }
+
+    h1{
+        color: var(--prussianBlue);
+        font-size: 65px;
+        margin-bottom: 35px;
+    }
+</style>
