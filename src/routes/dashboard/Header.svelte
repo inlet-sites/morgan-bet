20606c40ab98331322c4d455f477ff131860f6c3
Lee Morgan <lee@leemorgan.dev>
2025-02-28 16:47:06 -0500

    Create the mobile version of header.

--- src/routes/dashboard/Header.svelte
+++ src/routes/dashboard/Header.svelte
@@ -1,5 +1,27 @@
 <script>
     import logo from "$lib/images/logo.svg";
+    import {afterNavigate} from "$app/navigation";
+
+    let options = $state();
+    let menuOpen = $state(false);
+
+    afterNavigate(()=>{
+        menuOpen = false;
+        document.body.overflow = "auto";
+        options.style.display = "none";
+    });
+
+    const toggleMenu = ()=>{
+        if(menuOpen){
+            menuOpen = false;
+            document.body.overflow = "auto";
+            options.style.display = "none";
+        }else{
+            menuOpen = true;
+            document.body.overflow = "hidden";
+            options.style.display = "flex";
+        }
+    }
 </script>
 
 <header>
@@ -8,11 +30,19 @@
         <p>MorganBet</p>
     </a>
     
-    <div class="options">
+    <div class="options" bind:this={options}>
         <a href="/dashboard">My Games</a>
         <div class="divider"></div>
         <a href="/dashboard/find">Find Games</a>
     </div>
+
+    <button class="hamburger" aria-label="menu" onclick={toggleMenu}>
+        <svg width="35px" height="35px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" color="currentColor">
+            <path d="M3 5H21" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M3 12H21" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M3 19H21" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"></path>
+        </svg>
+    </button>
 </header>
 
 <style>
@@ -53,5 +83,45 @@
     .divider{
         border-right: 2px solid var(--claret);
         margin: 0 15px;
+    }
+
+    .hamburger{
+        display: none;
+        color: var(--scarlet);
+        background: none;
+        border: none;
+        margin-right: 35px;
+        cursor: pointer;
+        margin-left: auto;
+    }
+
+    @media screen and (max-width: 800px){
+        .options{
+            display: none;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            position: fixed;
+            top: 75px;
+            left: 0;
+            height: 100vh;
+            width: 100vw;
+            background: var(--prussianBlue);
+            margin: 0;
+            padding: 0 0 150px 0;
+        }
+
+        .options a{
+            margin: 35px;
+            font-size: 35px;
+        }
+
+        .divider{
+            display: none;
+        }
+
+        .hamburger{
+            display: flex;
+        }
     }
 </style>
